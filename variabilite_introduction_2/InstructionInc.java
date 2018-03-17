278d066ef871c681c9e90b8674045690cbcda6fb
douin <jean-michel.douin@cnam.fr>
2018-03-17 15:13:43 +0100

    trace supprimée

--- variabilite_introduction_2/InstructionInc.java
+++ variabilite_introduction_2/InstructionInc.java
@@ -3,7 +3,7 @@ package variabilite_introduction_2;
 import instructions.Instruction;
 
 public class InstructionInc<E,R> extends Instruction<Memoire,E,R>{
-      private static final boolean T = true;
+      private static final boolean T = false;
       
       public boolean executer(Memoire m, E e, R r){
           if(T)System.out.println("InstructionInc.executer: " + m);
